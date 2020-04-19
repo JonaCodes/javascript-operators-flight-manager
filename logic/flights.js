40c6e820325e75d2cf397e67bbabac308bb49ce8
jona <jona@elevation.ac>
2020-04-19 10:26:05 +0300

    correct

--- logic/flights.js
+++ logic/flights.js
@@ -1,8 +1,8 @@
 function Flights() {
     const calculateNumberOfFlights = function (numPassengers, capacity) {
-        const numFlights = numPassengers / capacity
+        const numFlights = Math.ceil(numPassengers / capacity)
         console.log(numFlights)
-        return numFlights % 2 === 0 ? numFlights : numFlights + 1
+        return numFlights % 2 == 0 ? numFlights : numFlights
     }
 
     return { calculateNumberOfFlights };
