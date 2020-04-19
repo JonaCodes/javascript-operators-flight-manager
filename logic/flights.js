da00b7c3982e88dcf87ec3a476c276a804110133
jona <jona@elevation.ac>
2020-04-19 10:26:51 +0300

    test

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,6 @@
 function Flights() {
     const calculateNumberOfFlights = function (numPassengers, capacity) {
         const numFlights = Math.ceil(numPassengers / capacity)
-        console.log(numFlights)
         return numFlights % 2 == 0 ? numFlights : numFlights
     }
 
